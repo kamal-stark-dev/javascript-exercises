cb8ecf3a78f1304543c2b768fbcd5fa23fe05aae
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-20 21:59:16 +0530

    add basic calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function () {
+  return arguments[0] + arguments[1];
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+  return arguments[0] - arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let sum = 0;
+  nums.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let mul = 1;
+  nums.forEach((element) => {
+    mul *= element;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  let ans = 1;
+  for (let i = 0; i < power; i++) {
+    ans *= base;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) return 1;
+  let fact = 1;
+  for (let i = num; i >= 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
